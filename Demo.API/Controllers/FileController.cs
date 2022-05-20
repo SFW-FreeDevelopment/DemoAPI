844a7d0ad18e8bf13c1b3e5f7ab91ce5369a6075
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-20 19:50:43 -0400

    Added File controller with Download and Upload actions

---
+++ Demo.API/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FileController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(IFormFile), StatusCodes.Status200OK)]
+    public IActionResult Download()
+    {
+        var obj = new
+        {
+            TestProp1 = true,
+            TestProp2 = "test value 2",
+            TestProp3 = 3 
+        };
+        var json = JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true });
+        var jsonBytes = Encoding.UTF8.GetBytes(json);
+        return File(jsonBytes, System.Net.Mime.MediaTypeNames.Application.Octet, "test.json");
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    public IActionResult Upload(IFormFile file)
+    {
+        Console.WriteLine($"File is {file.Length} bytes.");
+        return Accepted();
+    }
+}
